d37388988adc21d1e1807f7eee25c45f7370029f
jukitot <irzhyckaya@gmail.com>
2024-09-28 19:16:39 +0300

    lesson1_part1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, {FC} from 'react';
 import './App.css';
+import Character from "./components/character/Character";
 
 const App: FC = () => {
     return (
         <>
+            <Character/>
         </>
     );
 }
---
+++ src/components/character/Character.tsx
@@ -0,0 +1,15 @@
+import React, {FC} from 'react';
+
+const Character: FC = () => {
+    return (
+        <div>
+            <h2>Bart</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/ru/2/29/Bart_Simpson.gif" alt="bart_simpson"/>
+
+            <h2>Homer</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png" alt="homer_simpson"/>
+        </div>
+    );
+};
+
+export default Character;
